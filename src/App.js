3eaa7ad108d237bb2b0fa051c9884bef7560d9d1
Jose Torreblanca <josewhitetower@gmail.com>
2021-10-12 18:53:13 +0200

    added lights and shadows

--- src/App.js
+++ src/App.js
@@ -15,9 +15,28 @@ const Box = (props) => {
     ref.current.rotation.y += 0.01;
   });
   return (
-    <mesh ref={ref} {...props}>
+    <mesh ref={ref} {...props} castShadow>
       <boxBufferGeometry />
-      <meshBasicMaterial color="orange" />
+      <meshPhysicalMaterial color="orange" />
+    </mesh>
+  );
+};
+
+const Floor = (props) => {
+  return (
+    <mesh {...props} receiveShadow>
+      <boxBufferGeometry args={[20, 1, 10]} />
+      <meshPhysicalMaterial />
+    </mesh>
+  );
+};
+
+const Bulb = (props) => {
+  return (
+    <mesh {...props}>
+      <pointLight castShadow />
+      <sphereBufferGeometry args={[0.2, 20, 20]} />
+      <meshPhongMaterial emissive="yellow" />
     </mesh>
   );
 };
@@ -25,10 +44,14 @@ function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <Canvas
+        shadows
         style={{ backgroundColor: "black" }}
         camera={{ position: [3, 3, 3] }}
       >
+        <ambientLight intensity={0.2} />
+        <Bulb position={[0, 3, 0]} />
         <Box position={[-1, 1, 2]} />
+        <Floor position={[0, -0.5, 0]} />
         <axesHelper args={[5]} />
         <Orbit />
       </Canvas>
