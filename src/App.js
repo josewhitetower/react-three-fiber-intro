357965074850bd5ee5c9a7de037b379e062811a1
Jose Torreblanca <josewhitetower@gmail.com>
2021-10-12 17:51:45 +0200

    built scene with react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Box = () => {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="orange" />
+    </mesh>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas style={{ backgroundColor: "black" }}>
+        <Box />
+      </Canvas>
     </div>
   );
 }
